f90e538cdc141f3623f173ce770e5e4d4f5b2a1a
Hanif Ariffin <hanif.ariffin.4326@gmail.com>
2020-02-07 06:17:29 -0500

    Implemented a generalized BST search.

--- include/akarithm/problem_700.hpp
+++ include/akarithm/problem_700.hpp
@@ -2,8 +2,32 @@
 
 #include "data_structure/tree_node.hpp"
 
+#include <functional>
+
 namespace akarithm
 {
+
+template <
+    typename T,
+    typename EqualFunction = std::equal_to<>,
+    typename SearchFunction = std::less<>>
+static constexpr auto
+searchBST_Generalized(
+    const TreeNode<T> *root,
+    const T &val,
+    const EqualFunction &equal = EqualFunction(),
+    const SearchFunction &search = SearchFunction())
+    -> const TreeNode<T> *
+{
+    if (!root)
+        return nullptr;
+    else if (equal(root->val, val))
+        return root;
+    else if (search(root->val, val))
+        return searchBST_Generalized(root->right, val, equal, search);
+    else
+        return searchBST_Generalized(root->left, val, equal, search);
+}
 
 template <typename T>
 static constexpr auto
@@ -12,16 +36,7 @@ searchBST(
     const T &val)
     -> const TreeNode<T> *
 {
-    if (!root)
-        return nullptr;
-    else if (root->val == val)
-        return root;
-    else if (root->val < val)
-        return searchBST(root->right, val);
-    else if (root->val > val)
-        return searchBST(root->left, val);
-    else
-        return nullptr;
+    return searchBST_Generalized(root, val);
 }
 
 } // namespace akarithm
