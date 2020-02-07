d9c47f21be0a7343d7252c055a6ab1387a8e2c9d
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2020-02-07 13:34:02 -0500

    Solution that does not work in leetcode
    But works locally. The problem is that you need to use IntTy = std::size_t
    for it to work with n=1407 due to overflow.
    
    Not sure if this is "correct" or not...

--- include/akarithm/problem_264.hpp
+++ include/akarithm/problem_264.hpp
@@ -3,7 +3,7 @@
 #include <type_traits>
 #include <algorithm>
 #include <numeric>
-#include <set>
+#include <map>
 #include <array>
 #include <cmath>
 #include <vector>
@@ -15,7 +15,7 @@ namespace akarithm
 //  TODO    ::  This algorithm can be improved further by employing std::set
 //              as the container for DP.
 template <
-    typename IntTy,
+    typename IntTy = std::size_t,
     typename = std::enable_if_t<std::is_integral_v<IntTy>>>
 static constexpr auto
 nthUglyNumber(const IntTy &n)
@@ -26,7 +26,7 @@ nthUglyNumber(const IntTy &n)
 
     using TupleTy = typename std::array<IntTy, 3>;
     using TupleIndexTy = typename TupleTy::size_type;
-    using DpTy = typename std::set<TupleTy>;
+    using DpTy = typename std::map<IntTy, TupleTy>;
     using DpIndexTy = typename DpTy::size_type;
 
     auto evaluate_tuple = [](const TupleTy &tuple)
@@ -37,41 +37,43 @@ nthUglyNumber(const IntTy &n)
         return static_cast<IntTy>(pow2 * pow3 * pow5);
     };
 
-    DpTy dp = {{0, 0, 0}};
+    DpTy dp;
+    dp.emplace(std::make_pair(
+        evaluate_tuple({0, 0, 0}),
+        TupleTy{0, 0, 0}));
     for (DpIndexTy iter = 1; iter != static_cast<DpIndexTy>(n); iter++)
     {
-        // Push all possible combinations into the dp
-        std::vector<TupleTy> tmp;
-        tmp.reserve(dp.size() * 3);
-        for (const auto &[a, b, c] : dp)
-        {
-            tmp.push_back({a + 1, b, c});
-            tmp.push_back({a, b + 1, c});
-            tmp.push_back({a, b, c + 1});
-        }
-        tmp.reserve(tmp.size());
-        for (const TupleTy &t : tmp)
-            dp.emplace(t);
-
         // Find the lowest evaluation
         auto min =
             std::min_element(
                 std::begin(dp), std::end(dp),
-                [&](const TupleTy &lhs, const TupleTy &rhs)
+                [&](const auto &lhs, const auto &rhs)
                     -> bool {
-                    return evaluate_tuple(lhs) < evaluate_tuple(rhs);
+                    return lhs.first < rhs.first;
                 });
+        auto [a, b, c] = min->second;
+        // Create the next generation
+        dp.emplace(std::make_pair(
+            evaluate_tuple({a + 1, b, c}),
+            TupleTy{a + 1, b, c}));
+        dp.emplace(std::make_pair(
+            evaluate_tuple({a, b + 1, c}),
+            TupleTy{a, b + 1, c}));
+        dp.emplace(std::make_pair(
+            evaluate_tuple({a, b, c + 1}),
+            TupleTy{a, b, c + 1}));
 
         // Remove the lowest one from the dp
         dp.erase(min);
     }
 
-    return evaluate_tuple(*std::min_element(
-        std::begin(dp), std::end(dp),
-        [&](const TupleTy &lhs, const TupleTy &rhs)
-            -> bool {
-            return evaluate_tuple(lhs) < evaluate_tuple(rhs);
-        }));
+    return std::min_element(
+               std::begin(dp), std::end(dp),
+               [&](const auto &lhs, const auto &rhs)
+                   -> bool {
+                   return lhs.first < rhs.first;
+               })
+        ->first;
 }
 
 } // namespace akarithm
--- src/akarithm/problem_264.cpp
+++ src/akarithm/problem_264.cpp
@@ -6,33 +6,41 @@
 
 TEST_CASE("problem 264 1")
 {
-    const int input = 10;
-    const int expected = 12;
-    const int result = akarithm::nthUglyNumber(input);
+    const std::size_t input = 10;
+    const std::size_t expected = 12;
+    const std::size_t result = akarithm::nthUglyNumber(input);
     CHECK(expected == result);
 }
 
 TEST_CASE("problem 264 2")
 {
-    const int input = 306;
-    const int expected = 92160;
-    // const int result = akarithm::nthUglyNumber(input);
-    // CHECK(expected == result);
+    const std::size_t input = 306;
+    const std::size_t expected = 92160;
+    const std::size_t result = akarithm::nthUglyNumber(input);
+    CHECK(expected == result);
 }
 
 TEST_CASE("problem 264 3")
 {
-    const int input = 1;
-    const int expected = 1;
-    const int result = akarithm::nthUglyNumber(input);
+    const std::size_t input = 1407;
+    const std::size_t expected = 536870912;
+    const std::size_t result = akarithm::nthUglyNumber(input);
+    CHECK(expected == result);
+}
+
+TEST_CASE("problem 264 3")
+{
+    const std::size_t input = 1;
+    const std::size_t expected = 1;
+    const std::size_t result = akarithm::nthUglyNumber(input);
     CHECK(expected == result);
 }
 
 TEST_CASE("problem 264 4")
 {
-    const std::vector<int> input = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    const std::vector<int> expected = {1, 2, 3, 4, 5, 6, 8, 9, 10, 12};
+    const std::vector<std::size_t> input = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    const std::vector<std::size_t> expected = {1, 2, 3, 4, 5, 6, 8, 9, 10, 12};
     CHECK(input.size() == expected.size());
-    for (std::vector<int>::size_type iter = 0; iter != input.size(); iter++)
+    for (std::vector<std::size_t>::size_type iter = 0; iter != input.size(); iter++)
         CHECK(expected[iter] == akarithm::nthUglyNumber(input[iter]));
 }
