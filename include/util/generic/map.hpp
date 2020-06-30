64534d7d5d82828600dc6f769b9697b5ef4fbb01
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2020-06-30 14:12:21 -0400

    Updated map type checking

--- include/util/generic/map.hpp
+++ include/util/generic/map.hpp
@@ -4,16 +4,21 @@
 
 namespace aka {
 
-template<typename ResultTy = void, typename IterTy, typename MapFun>
+template<typename ResultTy,
+         typename IterTy,
+         typename MapFun,
+         typename ArgTy = typename std::iterator_traits<IterTy>::value_type>
 static constexpr auto
-map(IterTy iter_begin, IterTy iter_end, const MapFun& map) -> ResultTy
+map(IterTy iter_begin, IterTy iter_end, const MapFun& map)
+  -> std::conditional_t<std::is_same_v<ResultTy, void>,
+                        std::vector<std::invoke_result_t<MapFun, ArgTy>>,
+                        ResultTy>
 {
   ResultTy result;
   result.reserve(std::distance(iter_begin, iter_end));
 
-  while (iter_begin != iter_end) {
-    result.emplace_back(map(*iter_begin));
-    iter_begin = std::next(iter_begin);
+  for (auto iter = iter_begin; iter_begin != iter_end; iter++) {
+    result.emplace_back(map(*iter));
   }
 
   return result;
